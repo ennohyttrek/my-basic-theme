eb89375ed3be15898d631b94c3fd8cbc8d6334aa
Enno Hyttrek <ehy.global@gmail.com>
2020-12-02 14:52:37 +0100

    Changes

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title><?php bloginfo('title'); ?></title>
     <?php wp_head() ?>
 </head>
 
 <body>
-    
+    <h1><?php bloginfo('title'); ?></h1>
+
+    <?php while( have_posts() ) : the_post(); ?>
+    <h3><?php the_title(); ?></h3>
+    <?php endwhile; ?>
+
 </body>
 
 </html>
