c3b09653b4894b637c76b24857d5ef6a0dc03999
Enno Hyttrek <ehy.global@gmail.com>
2020-12-02 14:57:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,10 @@
     <h1><?php bloginfo('title'); ?></h1>
 
     <?php while( have_posts() ) : the_post(); ?>
-    <h3><?php the_title(); ?></h3>
+    <article>
+        <h3><?php the_title(); ?></h3>
+        <?php the_content(); ?>
+    </article>
     <?php endwhile; ?>
 
 </body>
